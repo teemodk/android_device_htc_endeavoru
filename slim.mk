dd24cd6b8804f148f072b27d542b90b7fc1d3ea7
teemodk <teemodk@gmail.com>
2013-10-30 19:23:31 +0100

    Create slim.mk

---
+++ slim.mk
@@ -0,0 +1,32 @@
+# Specify phone tech before including full_phone
+$(call inherit-product, vendor/slim/config/gsm.mk)
+
+# Release name
+PRODUCT_RELEASE_NAME := endeavoru
+
+# Preload bootanimation
+TARGET_BOOTANIMATION_PRELOAD := true
+PRODUCT_COPY_FILES += \
+    vendor/slim/prebuilt/hdpi/bootanimation.zip:system/media/bootanimation.zip
+
+# Inherit some common Slim stuff.
+$(call inherit-product, vendor/slim/config/common_full_phone.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/slim/config/nfc_enhanced.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/htc/endeavoru/full_endeavoru.mk)
+
+# Inherit torch settings
+$(call inherit-product, vendor/slim/config/common_ledflash.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := endeavoru
+PRODUCT_NAME := slim_endeavoru
+PRODUCT_BRAND := htc_europe
+PRODUCT_MODEL := HTC One X
+PRODUCT_MANUFACTURER := HTC
+
+#Set build fingerprint / ID / Product Name ect.
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=endeavoru TARGET_DEVICE=endeavoru BUILD_FINGERPRINT="htc_europe/endeavoru/endeavoru:4.1.1/JRO03C/128187.31:user/release-keys" PRIVATE_BUILD_DESC="3.14.401.31 CL128187 release-keys"
