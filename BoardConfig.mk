56b3c2348baa0da40d066f235b0896cf79a91647
Tomas Mogensen <teemodk@gmail.com>
2014-04-05 14:22:02 +0200

    use non-TI (aosp) supplicant

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -57,9 +57,11 @@ USES_TI_MAC80211 := true
 # Required for newer wpa_supplicant_8_ti versions to fix tethering
 BOARD_WIFI_SKIP_CAPABILITIES := true
 
-ifdef USES_TI_MAC80211
 BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
-WPA_SUPPLICANT_VERSION           := VER_0_8_X_TI
+WPA_SUPPLICANT_VERSION           := VER_0_8_X
+# Private libs for the non-TI wpa_supplicant
+BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_wl12xx
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_wl12xx
 BOARD_HOSTAPD_DRIVER             := NL80211
 BOARD_WLAN_DEVICE                := wl12xx_mac80211
 BOARD_SOFTAP_DEVICE              := wl12xx_mac80211
@@ -67,7 +69,6 @@ WIFI_DRIVER_MODULE_PATH          := "/system/lib/modules/wl12xx_sdio.ko"
 WIFI_DRIVER_MODULE_NAME          := "wl12xx_sdio"
 WIFI_FIRMWARE_LOADER             := ""
 COMMON_GLOBAL_CFLAGS             += -DUSES_TI_MAC80211
-endif
 
 # Kernel
 TARGET_KERNEL_SOURCE := kernel/htc/endeavoru
@@ -144,7 +145,6 @@ BOARD_SEPOLICY_DIRS += \
     device/htc/endeavoru/sepolicy
 
 BOARD_SEPOLICY_UNION += \
-     app.te             \
      device.te          \
      file_contexts      \
      drmserver.te       \
