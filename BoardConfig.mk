99abf280e19cba798a2d65b6b54bf72aa3adacaa
Tomas Mogensen <teemodk@gmail.com>
2015-06-13 14:08:38 +0200

    add twrp flags

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -118,6 +118,28 @@ TARGET_RECOVERY_FSTAB := device/htc/endeavoru/ramdisk/fstab.endeavoru
 RECOVERY_FSTAB_VERSION := 2
 BOARD_HAS_LARGE_FILESYSTEM := true
 
+#TWRP CONFIG:
+#DEVICE_RESOLUTION to be eliminated: https://github.com/TeamWin/Team-Win-Recovery-Project/commit/591b920
+DEVICE_RESOLUTION := 720x1280
+# new handling of resolution
+TW_THEME := portrait_hdpi
+# this enables proper handling of /data/media on devices that have this folder for storage
+RECOVERY_SDCARD_ON_DATA := true
+# disables things like sdcard partitioning
+BOARD_HAS_NO_REAL_SDCARD := true
+# removes the USB storage button on devices that don't support USB storage
+TW_NO_USB_STORAGE := true
+TW_BRIGHTNESS_PATH := /sys/devices/platform/tegra-pwm-bl/backlight/tegra-pwm-bl/brightness
+TW_MAX_BRIGHTNESS := 255
+TW_NO_SCREEN_BLANK := true
+# fixes slanty looking graphics on some devices
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+HAVE_SELINUX := true
+TWHAVE_SELINUX := true
+# cut the size of twrp
+# TW_INCLUDE_JB_CRYPTO := true
+TW_EXCLUDE_SUPERSU := true
+
 # UMS
 TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/class/android_usb/f_mass_storage/lun0/file"
 TARGET_RECOVERY_DEVICE_MODULES += chargeled
